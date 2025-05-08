62fcdac66ec69d7b658e9a2a11cac0d20f80b20e
Emil <erazertt@gmail.com>
2025-05-08 12:37:39 +0200

    Initial commit

---
+++ src/main/java/com/example/projectgilbert/ProjectGilbertApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectgilbert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectGilbertApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectGilbertApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/projectgilbert/ProjectGilbertApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.projectgilbert;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectGilbertApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
